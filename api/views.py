7e1bcb2384d815f44562f4ba9707247ff0f5daac
matt700395 <matt7003@korea.ac.kr>
2022-01-27 12:00:10 +0900

    base setting for drf tutorial

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def apiOverview(request):
+    '''url 패턴을 모두 보여주는 view임, Json 패턴을 dictionary형태의 데이털 뿐임'''
+    return JsonResponse("API BASE POINT", safe=False)

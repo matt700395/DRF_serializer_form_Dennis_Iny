5ee72c35e75dd4ce92315e6633b50736de3ebe8d
matt700395 <matt7003@korea.ac.kr>
2022-01-27 12:30:00 +0900

    migrate

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from api import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
-    path('task-list', views.taskList, name="task-list")
+    path('task-list/', views.taskList, name="task-list")
 ]

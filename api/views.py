15fc0c5951833cce0ee31269b3b112536e3fb923
matt700395 <matt7003@korea.ac.kr>
2022-01-27 12:05:57 +0900

    apiOverview

--- api/views.py
+++ api/views.py
@@ -1,8 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+'''url 패턴을 모두 보여주는 view임, Json 패턴을 dictionary형태의 데이털 뿐임'''
+@api_view(['GET'])
 def apiOverview(request):
-    '''url 패턴을 모두 보여주는 view임, Json 패턴을 dictionary형태의 데이털 뿐임'''
-    return JsonResponse("API BASE POINT", safe=False)
+
+	api_urls = {
+		'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)

fd24c5c34e796a9ad00ccac0dbbcf4c1637f2b99
matt700395 <matt7003@korea.ac.kr>
2022-01-27 12:23:34 +0900

    task list serializer make

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview")
+    path('', views.apiOverview, name="api-overview"),
+    path('task-list', views.taskList, name="task-list")
 ]
--- api/views.py
+++ api/views.py
@@ -6,6 +6,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from api.models import Task
+from api.serializers import TaskSerializer
+
 '''url 패턴을 모두 보여주는 view임, Json 패턴을 dictionary형태의 데이털 뿐임'''
 @api_view(['GET'])
 def apiOverview(request):
@@ -19,3 +22,10 @@ def apiOverview(request):
 		}
 
 	return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+	tasks = Task.objects.all() #model에서 Task 데이터를 가져옴
+	serializer = TaskSerializer(tasks, many=True)# 가져온 데이터를 Serializer로 serializing함
+	return Response(serializer.data) #serializing된 데이터를 response로 내보냄
+	
